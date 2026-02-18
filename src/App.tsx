238ee5061f76461599d9ae3636d3bc0a8d15a75e
Dmytro Odnoroh <falcon121299@icloud.com>
2026-02-18 19:10:43 +0200

    shop page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+/* eslint max-len: "off" */
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { CartAndFavouritesProvider } from './components/context/CartAndFavouritesContext';
+import { PathProvider } from './components/context/PathContext';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { CartPage } from './pages/CartPage';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { ProductsPage } from './pages/ProductsPage';
+import { ProductInfoPage } from './pages/ProductInfoPage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <CartAndFavouritesProvider>
+      <PathProvider>
+        <div className="app">
+          <Header />
+          <main className="main">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/favourites" element={<FavouritesPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route
+                path="/product-not-found"
+                element={<NotFoundPage type="product" />}
+              />
+              <Route path="/not-found" element={<NotFoundPage type="page" />} />
+              <Route path=":category">
+                <Route index element={<ProductsPage />} />
+                <Route path=":itemId" element={<ProductInfoPage />} />
+              </Route>
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </PathProvider>
+    </CartAndFavouritesProvider>
   );
 };
